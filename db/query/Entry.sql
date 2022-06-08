8b8117a4b2ad1ae191b463717f29187311764d05
Sourav Sharma <souravgopal25@gmail.com>
2022-06-08 23:29:08 +0530

    Code Geerated for Entry

---
+++ db/query/Entry.sql
@@ -0,0 +1,32 @@
+-- name: CreateEntry :one
+INSERT INTO entries(
+        account_id, amount
+) values (
+          $1 , $2
+          )
+returning *;
+
+-- name: GetEntry :one
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1;
+
+-- name: GetAllEntriesOfAccount :many
+SELECT * FROM entries
+WHERE account_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
+
+-- name: UpdateEntry :one
+UPDATE entries
+SET account_id = $1,
+    amount=$2
+WHERE id=$3
+RETURNING *;
+
+-- name: DeleteEntry :exec
+DELETE FROM entries
+WHERE id = $1;
+
+
+
