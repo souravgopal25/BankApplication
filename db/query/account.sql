a001f366e87b3e0f8bd32f4751f548a9d9bdd644
Sourav Sharma <souravgopal25@gmail.com>
2022-06-08 23:09:34 +0530

    Code Geerated for Account

--- db/query/account.sql
+++ db/query/account.sql
@@ -6,3 +6,23 @@ INSERT INTO account (
              $1, $2, $3
          )
 RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM account
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM account
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :one
+UPDATE account
+set balnce = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM account
+       WHERE id =$1;
