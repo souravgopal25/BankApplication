15465a595b2a10820352661236fb4ec9c6c763d7
Sourav Sharma <souravgopal25@gmail.com>
2022-06-10 02:09:18 +0530

    account_test.go complete

--- util/random.go
+++ util/random.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"math/rand"
+	"strings"
 	"time"
 )
 
@@ -15,4 +16,32 @@ func init() {
 //RandomInt generates a random Integer bw min and max
 func RandomInt(min, max int64) int64 {
 	return min + rand.Int63n(max-min+1)
+}
+
+//RandomString generates a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	l := len(alphabet)
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(l)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+//RandomMoney generates a random amount
+func RandomMoney() int64 {
+	return RandomInt(0, 10000)
+}
+
+//RandomCurrency Generator
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "INR"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
 }
